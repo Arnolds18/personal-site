457f2a12e729660c61664f0afba21913768a041e
arnoldsidiprasetija <arnolds1809@gmail.com>
2022-12-13 15:18:49 +0700

    refactor: change author

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -19,13 +19,13 @@ const Main = ({ children, router }) => {
         <meta name="author" content="Arnold Sidiprasetija" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="TakArnold Sidiprasetija" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@craftzdog" />
         <meta name="twitter:creator" content="@craftzdog" />
         <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta property="og:site_name" content="TakArnold Sidiprasetija" />
+        <meta name="og:title" content="TakArnold Sidiprasetija" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.craftz.dog/card.png" />
         <title>Arnold Sidiprasetija</title>
